ad7f675d2096003fc6dfed53b16e0a28914717da
Simone Vendramini <102531922+Svendra4UniMiB@users.noreply.github.com>
2025-01-29 14:40:13 +0100

    Outlier Detection

--- Scripts/DatasetCleaning.py
+++ Scripts/DatasetCleaning.py
@@ -1,18 +1,61 @@
 from ImageDatastore import ImageDatastore
-from Utils import create_or_clear_directory
-from ExtractFeatures import ExtractFeatures
+from Utils import create_or_clear_directory, transform
+from NeuralFeatureExtractor import MobileNetFeatureExtractor
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
+from sklearn.cluster import DBSCAN
+from scipy.spatial.distance import pdist
+import numpy as np
+import os
+import shutil
+from sklearn.preprocessing import StandardScaler
 
 
-train_data = ImageDatastore('train', transform=ToTensor())
+train_data = ImageDatastore('train', transform=transform(256))
 
 batch_size = 512
 train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-create_or_clear_directory('../Results/outliers')
+extractor = MobileNetFeatureExtractor()
+features, labels = extractor.compute_features(train_dataloader)
 
-extractor = ExtractFeatures()
-features, labels = extractor.extract_featuresAlexNet(train_dataloader)
+path_outliers = '../Results/outliers2'
+create_or_clear_directory(path_outliers)
 
-# print(f"N elements {len(features)}, N feature {len(features[0][2])}")
+valid_image_idx = list(range(len(features)))
+
+range_labels = len(features) // 20
+outliers = []
+
+scaler = StandardScaler()
+
+for i in range(range_labels):
+    
+    current_features = features[i*20:(i+1)*20, :]
+
+    features_scaled = scaler.fit_transform(current_features)
+
+    distanze = pdist(features_scaled, 'euclidean')
+    
+    epsilon = np.median(distanze)
+    min_pts = 2
+    
+    dbscan = DBSCAN(eps=epsilon, min_samples=min_pts)
+    labels = dbscan.fit_predict(features_scaled)
+    
+    outlier_idx = np.where(labels == -1)[0]
+
+    if outlier_idx.size == 0:
+        outlier_idx = np.array([0])
+    else:
+        for idx in outlier_idx:
+            img_idx = i * 20 + idx
+            img_name = train_data.labels.iloc[img_idx][0]
+            img_path = os.path.join(train_data.images_directory, img_name)
+
+            valid_image_idx.remove(img_idx)
+            
+            file_name, file_ext = os.path.splitext(img_name)
+            outlier_file_name = os.path.join(path_outliers, f"{i}_class_{file_name}{file_ext}")
+            
+            shutil.copy2(img_path, outlier_file_name)
+    outliers.extend(outlier_idx + i * 20)
--- Scripts/Utils.py
+++ Scripts/Utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from torchvision import transforms
 
 def create_or_clear_directory(output_folder):
 
@@ -12,3 +13,14 @@ def create_or_clear_directory(output_folder):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
+
+def transform(input_size):
+    transform = transforms.Compose(
+        [
+            transforms.Resize(
+                (input_size, input_size), interpolation=transforms.InterpolationMode.BILINEAR
+            ),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    return transform
