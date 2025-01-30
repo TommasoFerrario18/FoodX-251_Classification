24080bfc9fbfafbcb5db0ee8759dda7005cd0b32
Tommaso Ferrario <tommasoferrario81@gmail.com>
2025-01-30 14:00:51 +0100

    Esperimenti Retrieval

--- Scripts/ImageRetrieval.py
+++ Scripts/ImageRetrieval.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy as sp
 
 from tqdm import tqdm
+from statistics import mode
 from abc import ABC, abstractmethod
 from sklearn.neighbors import NearestNeighbors
 
@@ -23,6 +24,22 @@ class ImageRetrieval(ABC):
             for image, label in zip(images, labels):
                 f.write(f"{image},{label}\n")
 
+class BaseRetrieval(ImageRetrieval):
+    def __init__(self, query_features, query_labels, k=5, metric="cosine"):
+        super().__init__(query_features, query_labels)
+        self.k = k
+        self.metric = metric
+    
+    def retrieve_images(self, images):
+        """Retrieve the k nearest images to the query images"""
+        neigh = NearestNeighbors(n_neighbors=self.k, n_jobs=-1, metric=self.metric)
+        neigh.fit(images)
+        
+        distances, indices = neigh.kneighbors(self.query_features)
+        
+        return distances, indices
+    
+    
 
 class KNNRetrieval(ImageRetrieval):
     def __init__(self, query_features, query_labels, k=5):
@@ -39,13 +56,18 @@ class KNNRetrieval(ImageRetrieval):
 
         distances, indices = neigh.kneighbors(images)
 
-        return self._augment_dataset(distances, indices, metric)
+        return self._augment_dataset2(distances, indices, metric)
 
     def _augment_dataset(self, distances, indices, metric="cosine"):
         """Augment the dataset with the query images"""
         new_labels = np.zeros(indices.shape[0], dtype=np.int32) - 1
         seen = set()
-        for i in tqdm(range(self.min_class, self.max_class + 1)):
+        for i in tqdm(
+            range(
+                self.min_class,
+                self.max_class + 1,
+            )
+        ):
             # Calcola le features e la distanza mediana della classe una sola volta
             idxs = np.where(self.query_labels == i)[0]
             class_feat = self.query_features[idxs]
@@ -77,6 +99,39 @@ class KNNRetrieval(ImageRetrieval):
                     seen.add(j)
 
         return new_labels
+
+    def _augment_dataset2(self, distances, indices, metric="cosine"):
+        """Augment the dataset with the query images"""
+        new_labels = np.full(indices.shape[0], -1, dtype=np.int32)
+
+        labels = []
+        for idx in indices:
+            labels.append(self.query_labels[idx])
+
+        medians = np.full(self.max_class - self.min_class + 1, -1, dtype=np.float32)
+
+        seen = set()
+        for i, lab in tqdm(enumerate(labels)):
+            _class = mode(lab)
+
+            idxs = np.where(self.query_labels == _class)[0]
+            class_feat = self.query_features[idxs]
+
+            if medians[_class] == -1:
+                medians[_class] = np.median(
+                    sp.spatial.distance.pdist(class_feat, metric=metric)
+                )
+            if np.count_nonzero(lab == _class) >= 3:
+                positions = [i for i, x in enumerate(lab) if x == _class]
+                d = distances[positions]
+
+                if np.any(d <= medians[_class]):
+                    new_labels[i] = _class
+                    seen.add(i)
+        return new_labels
+
+    def _expand_less_frequent_class():
+        pass
 
 
 class FAISSRetrieval(ImageRetrieval):
